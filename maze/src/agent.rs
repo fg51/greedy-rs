2d5739c8eb2813240706507fad4aee78ed5c3655
kflange <zknsftk1msrk@gmail.com>
2020-05-19 19:05:16 +0900

    update

--- maze/src/agent.rs
+++ maze/src/agent.rs
@@ -1,68 +1,81 @@
 use rand::{rngs::ThreadRng, thread_rng, Rng};
 
 use crate::direction::Direction;
+use crate::{Reward, State};
 
 use super::{EPISODE_MAX, NUM_OF_ACTION, NUM_OF_STATE};
 
 pub struct Agent {
-    start: u32,
-    goal: u32,
+    start: State,
+    is_finished: bool,
     learn_ratio: f64,
     discount_ratio: f64,
     q_values: [[f64; NUM_OF_ACTION]; NUM_OF_STATE],
     rand: ThreadRng,
+    target: f64,
 }
 
 impl Agent {
-    pub fn new(learn_ratio: f64, discount_ratio: f64) -> Self {
+    pub fn new(learn_ratio: f64, discount_ratio: f64, target: Reward) -> Self {
         Self {
             learn_ratio,
             discount_ratio,
-            start: 1,
-            goal: 17,
+            start: 1u32.into(),
+            is_finished: false,
             q_values: std::default::Default::default(),
             rand: thread_rng(),
+            target: target.0,
         }
     }
 
-    pub fn start(&self) -> u32 {
-        self.start
+    pub fn reset(&mut self) {
+        self.is_finished = false;
     }
 
-    pub fn goal(&self) -> u32 {
-        self.goal
+    pub fn start(&self) -> State {
+        self.start.into()
     }
 
-    pub fn next_action(&mut self, s: u32, episode: usize) -> Direction {
-        let p: f64 = self.rand.gen();
+    pub fn is_finished(&self) -> bool {
+        self.is_finished
+    }
+
+    pub fn next_action(&mut self, state: State, episode: usize) -> Direction {
         let epsilon = (EPISODE_MAX - episode) as f64 / EPISODE_MAX as f64;
-        Direction::new(if p <= epsilon {
-            self.rand.gen::<u32>()
+        if self.rand.gen::<f64>() <= epsilon {
+            self.rand.gen::<u32>().into()
         } else {
-            self.get_max_action(s)
-        })
+            self.get_max_action(state).into()
+        }
     }
 
-    pub fn update_q(&mut self, state: u32, action: &Direction, next_state: u32) {
-        let (state, action) = (state as usize, action.clone() as usize);
-        let reward = if next_state == self.goal {
-            1.0
-        } else if next_state == state as u32 {
-            -1.0
-        } else {
-            let max_action = self.get_max_action(next_state) as usize;
-            self.learn_ratio
-                * (self.discount_ratio * self.q_values[next_state as usize][max_action]
-                    - self.q_values[state][action])
-        };
-        self.q_values[state][action] += reward;
+    pub fn update_q(
+        &mut self,
+        state: State,
+        action: Direction,
+        next_state: State,
+        reward: Reward,
+    ) {
+        let reward: f64 = reward.into();
+        if reward == self.target {
+            self.is_finished = true;
+        }
+
+        let max_q =
+            self.get_q_value(next_state.into(), self.get_max_action(next_state.into()));
+        let q = self.get_q_value(state, action);
+
+        let delta_q: f64 = reward + self.discount_ratio * max_q - q;
+        let state: usize = state.into();
+        let action = action as usize;
+        self.q_values[state][action] += self.learn_ratio * delta_q;
     }
 
     // NOTE: Return the action that maximizes the Q-value at the current position.
-    pub fn get_max_action(&self, state: u32) -> u32 {
-        let state = state as usize;
+    pub fn get_max_action(&self, state: State) -> Direction {
+        let state: usize = state.into();
         let mut max_action: u32 = 0;
-        let mut max = -100000.0;
+        let mut max = -std::f64::INFINITY;
 
         let actions = self.q_values[state];
         for i in 0..NUM_OF_ACTION {
@@ -71,10 +84,11 @@ impl Agent {
                 max_action = i as u32;
             }
         }
-        return max_action;
+        return max_action.into();
     }
 
-    pub fn get_q_value(&self, state: u32, action: u32) -> f64 {
-        self.q_values[state as usize][action as usize]
+    pub fn get_q_value(&self, state: State, action: Direction) -> f64 {
+        let (state, action): (usize, u32) = (state.into(), action.into());
+        self.q_values[state][action as usize]
     }
 }
